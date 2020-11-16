b4935f6cab01ecf45068b9cc73074bd5e7e4d3ce
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-11-16 14:59:46 +0100

    begonnen aan 03-02

--- 03-component-communication/exercise-02/to-do-list.js
+++ 03-component-communication/exercise-02/to-do-list.js
@@ -1,18 +1,24 @@
-const template = document.createElement('template')
+const template = document.createElement('template');
 template.innerHTML = `
     <h1>To do</h1>
     <ul id="todos"></ul>
 `;
 
-
 class TodoList extends HTMLElement {
-    constructor() {
-        super();
-    }
+	constructor() {
+		super();
+	}
 
-    connectedCallback() {
-        this.appendChild(template.content.cloneNode(true))
-    }
+	connectedCallback() {
+		this.appendChild(template.content.cloneNode(true));
+		let ul = this.querySelector("#todos");
+		console.log(this.todos);
+		for(let i=0; i<list.length;i++){
+			let li = document.createElement("li");
+			li.innerText = list[i].text;
+			ul.appendChild(li);
+		}
+	}
+
 }
-
 window.customElements.define('to-do-list', TodoList);
