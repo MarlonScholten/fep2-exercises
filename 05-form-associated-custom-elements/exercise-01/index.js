8c0e6bbc74b7e734e1ed8ffd74c940fdb573a3b4
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-12-01 15:30:56 +0100

    exercise 05-01 gemaakt

--- 05-form-associated-custom-elements/exercise-01/index.js
+++ 05-form-associated-custom-elements/exercise-01/index.js
@@ -8,25 +8,46 @@ class MyCounter extends HTMLElement {
     constructor() {
         super();
         this.internals_ = this.attachInternals();
+        this._min = this.min;
         this.innerHTML = `
         <button type="button" id="decreaseBtn">-</button>
-        <input value="0" type="number">
+        <input value="0" type="number" min="${this._min}">
         <button type="button" id="increaseBtn">+</button>
     `;
 
         // add event listeners on both buttons
         this.querySelector('#increaseBtn').addEventListener('click', this.increase.bind(this));
         this.querySelector('#decreaseBtn').addEventListener('click', this.decrease.bind(this));
+
+        // bewijs dat formdata met juiste value word meegestuurd
+        const parentForm = this.parentNode;
+        parentForm.addEventListener('submit', event =>{
+            event.preventDefault();
+
+            const formData = new FormData(parentForm);
+            formData.append('counterValue',this.value);
+            for(let entry of formData.entries()){
+                console.log({
+                    key : entry[0],
+                    value : entry[1]
+                })
+            }
+        });
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === "step") {
             this.step = newValue;
         }
+        if (name === "min") {
+            this.min = newValue;
+        }
     }
 
-    // Form controls usually expose a "value"  property
-    get value() { return this.querySelector('input').value; }
+    // Form controls usually expose a "value" property
+    get value() {
+        return this.querySelector('input').value;
+    }
 
     set value(val) {
         this.querySelector('input').value = val;
@@ -40,14 +61,22 @@ class MyCounter extends HTMLElement {
         this._step = val;
     }
 
+    get min(){
+        return this.getAttribute('min');
+    }
+
+    set min(val){
+        this._min = val;
+    }
+
     increase() {
         this.value = Number(this.value) + Number(this._step);
-        this.validate();
+        this.querySelector('input').setAttribute('value', this.value);
     }
 
     decrease() {
         this.value = Number(this.value) - Number(this._step);
-        this.validate();
+        this.querySelector('input').setAttribute('value', this.value);
     }
 }
 
